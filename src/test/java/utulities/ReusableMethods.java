bf1c16fc481494aeb3360438500ff30d5243d56e
Murat <muratkorkmaz8306@gmail.com>
2023-02-28 20:42:01 +0100

    day10

---
+++ src/test/java/utulities/ReusableMethods.java
@@ -0,0 +1,13 @@
+package utulities;
+
+public class ReusableMethods {
+
+    public static void bekle(int saniye){
+
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
