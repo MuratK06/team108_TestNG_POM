7126f6c600055f073b80b31497031c08d30b2516
Murat <muratkorkmaz8306@gmail.com>
2023-03-01 15:57:13 +0100

    day10

---
+++ src/test/java/pages/FacebookPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utulities.Driver;
+
+public class FacebookPage {
+
+    public FacebookPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//button[@class='_42ft _4jy0 _9xo7 _4jy3 _4jy1 selected _51sy']")
+    public WebElement cookiesKabulButonu;
+
+    @FindBy(xpath = "//input[@class='inputtext _55r1 _6luy']")
+    public WebElement emailKutusu;
+
+    @FindBy(xpath = "//input[@class='inputtext _55r1 _6luy _9npi']")
+    public WebElement passwordKutusu;
+
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement loginButonu;
+
+    @FindBy(xpath = "//div[@class='_9ay7']")
+    public WebElement girisYapilamadiYaziElementi;
+}
--- src/test/java/tests/day10_TestNGFramework/C01_Priority.java
+++ src/test/java/tests/day10_TestNGFramework/C01_Priority.java
@@ -1,4 +1,4 @@
-package tests;
+package tests.day10_TestNGFramework;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
